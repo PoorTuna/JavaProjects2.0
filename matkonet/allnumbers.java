1c641a65deedf6491014b6e4c3019bed1debd97b
Oren <74069593+PoorTuna@users.noreply.github.com>
2021-01-07 17:03:55 +0200

    Update allnumbers.java

--- matkonet/allnumbers.java
+++ matkonet/allnumbers.java
@@ -12,7 +12,7 @@ public class allnumbers
 	}
 	public static void main(String[] args)
 	{
-		int[] sex = {1,2,3,4,5,6,7,8,9,10};
-		System.out.println(lastOddValue(sex));
+		int[] int_list = {1,2,3,4,5,6,7,8,9,10};
+		System.out.println(lastOddValue(int_list));
 	}
 }
